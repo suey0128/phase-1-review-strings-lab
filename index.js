52bb592f2ab16d2b94d97b0f56f538c937244ca5
suey0128 <syui0128@gmail.com>
2021-05-17 10:45:12 -0700

    commit tree e562c363d8d985982bd1d63bec499dc7103614ec

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'Suey'; 
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); 
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`; 
